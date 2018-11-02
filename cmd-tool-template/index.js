3e4b0ac042153e8684c2b5040652a4623a892e2b
Augustine von Trapp <augustvontrapp@gmail.com>
2018-11-02 15:09:45 -0700

    times table complete

--- cmd-tool-template/index.js
+++ cmd-tool-template/index.js
@@ -1,20 +1,50 @@
-const prompts = require('prompts');
-const fs = require('fs');
 // const questions = require('./questions');
-const questions = require('./questions.json');
+// const questions = require('./questions.json');
+const prompts = require('prompts');
+const bank = require('./bank.json');
+const fs = require('fs');
+const questions = require('./questions.js');
+
+Array.prototype.shuffle = function() {
+  let input = this;
+  
+  input.forEach((item, i) => {
+    const randomIndex = Math.floor(Math.random() * (i +1));
+
+    input[randomIndex] = input[i];
+    input[i] = item;
+  });
+
+  return input;
+}
+
 const format = (data) => {
-  return JSON.stringify(data);
+  return data.shuffle().map((data) => {
+    return {
+      type: 'text',
+      name: data.prompt,
+      message: data.prompt,
+      validate: (value) => value !== data.answer ? false : true,
+      initial: 'what is this?',
+    };
+  });
 };
 
 const main = async () => {
-  const results = await prompts(questions);
-  const formattedData = format(results);
-
-  fs.writeFile('bank.json', formattedData, 'utf8', (err) => {
-    if (err) {
-      return console.log('choo');
-    }
-  });
+  const  parsedData = format(bank);
+  const results = prompts(parsedData);
 }
 
 main();
+
+// const data = [];
+// for( let i = 1; i < 13; i++) {
+//   for( let j = 1; j < 13; j++) {
+//     data.push(
+//       {
+//         prompt: `${i} x ${j}`,
+//         answer: `${i*j}`
+//       }
+//     )
+//   }
+// }
