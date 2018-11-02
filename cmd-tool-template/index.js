098a301e220b8565132239acbd75e518a6f9daad
Augustine von Trapp <augustvontrapp@gmail.com>
2018-11-02 16:02:00 -0700

    switch to inquierer

--- cmd-tool-template/index.js
+++ cmd-tool-template/index.js
@@ -1,13 +1,16 @@
-const prompts = require('prompts');
+// const prompts = require('prompts');
+const inquirer = require('inquirer');
+const prompt = inquirer.prompt;
 const fs = require('fs');
-// const questions = require('./questions');
-const questions = require('./questions.json');
+const questions = require('./questions');
+// const questions = require('./questions.json');
+
 const format = (data) => {
   return JSON.stringify(data);
 };
 
 const main = async () => {
-  const results = await prompts(questions);
+  const results = await prompt(questions);
   const formattedData = format(results);
 
   fs.writeFile('bank.json', formattedData, 'utf8', (err) => {
--- cmd-tool-template/questions.js
+++ cmd-tool-template/questions.js
@@ -1,9 +1,7 @@
 const questions = [{
-    type: 'text',
-    name: 'input1',
-    message: 'What in the first input?',
-    validate: (value) => value === 'invalid' ? false : true,
-    initial: 'what is this?',
-  }];
+    type: 'input',
+    message: 'Hello?',
+    name: 'hello'
+}];
 
 module.exports = questions;
