77dec0f548a4e1101aa9c41f4170b786cbe81ec8
Augustine  von Trapp <augustinevontrapp@Augustines-MBP.domain>
2018-11-04 22:31:16 -0800

    complete most of flowchart for questions

--- cmd-tool-template/index.js
+++ cmd-tool-template/index.js
@@ -5,31 +5,78 @@ const prompts = inquirer.prompt;
 
 const flowOptions = {
   "0": {
-    "message": 'Do you know what I did last summer?',
+    "message": 'Find the greatest common factor',
     "choices": {
-      "yes": "1",
-      "no": "2",
+      "ok": "1",
     }
   },
   "1": {
-    "message": 'Really?',
+    "message": 'choose type?',
     "choices": {
-      "yes": "done",
-      "no": "done",
+      "Binomial": "2",
+      "Trinomial": "10",
+      "Polynomail Greater than three terms": "8",
+    }
+  },
+  "10": {
+    "message": 'decern information from the middle and last term signs',
+    "choices": {
+      "yes, done": "5",
     }
   },
   "2": {
-    "message": 'Good',
+    "message": 'Is the difference between two squares? (a^2 - b^2)',
     "choices": {
-      "cool, I'm going to go now": "3",
-      "no": "done",
+      "yes": "answer:0",
+      "no": "3",
     }
   },
   "3": {
-    "message": 'Are you sure you don\'t know anything?',
+    "message": 'Is the difference of two cubes? (a^3 - b^3)',
     "choices": {
-      "yes": "done",
+      "yes": "answer:1",
+      "no": "4",
+    }
+  },
+  "4": {
+    "message": 'Is the sum of two cubes? (a^3 + b^3)',
+    "choices": {
+      "yes": "answer:2",
       "no": "done",
+    }
+  },
+  "5": {
+    "message": 'Are the first and last terms squares? (x^2 - 12x + 36)',
+    "choices": {
+      "yes": "9",
+      "no": "6",
+    }
+  },
+  "9": {
+    "message": 'is the middle term equal to two times the product first and last term? ( ex: 2(6x) = 12x )',
+    "choices": {
+      "yes": "answer:3",
+      "no": "6",
+    }
+  },
+  "6": {
+    "message": 'Is the coefficient of the first term equal to one? (x^2 - 14x − 40)',
+    "choices": {
+      "yes": "answer:4",
+      "no": "7",
+    }
+  },
+  "7": {
+    "message": 'Is the coefficient of the first term greater than one? (2x^2 - 14x − 40)',
+    "choices": {
+      "yes": "answer:5",
+      "no": "done",
+    }
+  },
+  "8": {
+    "message": 'Factor by grouping and return to step two',
+    "choices": {
+      "m'kay": "2",
     }
   },
 }
