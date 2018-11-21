f2688fb8b0d20d57bd46ba6cdc4f1e66ecb8f626
Augustine von Trapp <augustvontrapp@gmail.com>
2018-11-21 15:21:08 -0800

    setup flowcharts as seperate JSON files

--- cmd-tool-template/index.js
+++ cmd-tool-template/index.js
@@ -1,118 +1,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-
+const { motivation, weirdRoom } = require('./sets/index');
 const prompts = inquirer.prompt;
 
+const flowchartMap = { motivation, weirdRoom };
 
-
-const flowchartOptions = {
-  "0": {
-    "message": 'Choose a flowchart',
-    "choices": {
-      "A": "a",
-      "b": "b",
-      "c": "c",
-    }
-  },
-}
-
-const a = {
-  "0": {
-    "message": 'a flowchart?',
-    "choices": {
-      "yes": "1",
-      "no": "2",
-    }
-  },
-  "1": {
-    "message": 'Really?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-  "2": {
-    "message": 'Good',
-    "choices": {
-      "cool, I'm going to go now": "3",
-      "no": "done",
-    }
-  },
-  "3": {
-    "message": 'Are you sure you don\'t know anything?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-}
-const b = {
-  "0": {
-    "message": 'b flowcahrt',
-    "choices": {
-      "yes": "1",
-      "no": "2",
-    }
-  },
-  "1": {
-    "message": 'Really?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-  "2": {
-    "message": 'Good',
-    "choices": {
-      "cool, I'm going to go now": "3",
-      "no": "done",
-    }
-  },
-  "3": {
-    "message": 'Are you sure you don\'t know anything?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-}
-const c = {
-  "0": {
-    "message": 'C flowchart',
-    "choices": {
-      "yes": "1",
-      "no": "2",
-    }
-  },
-  "1": {
-    "message": 'Really?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-  "2": {
-    "message": 'Good',
-    "choices": {
-      "cool, I'm going to go now": "3",
-      "no": "done",
-    }
-  },
-  "3": {
-    "message": 'Are you sure you don\'t know anything?',
-    "choices": {
-      "yes": "done",
-      "no": "done",
-    }
-  },
-}
-
-const flowchartMap = {
-  a,
-  b,
-  c
-}
-
+// EXECUTE FLOWCHART
 const executeFlowchart = async (name, message="wha?", flowchart, type='list') => {
   const path = name.split('/');
   const flowOption = flowchart[path[path.length - 1]];
@@ -129,20 +22,21 @@ const executeFlowchart = async (name, message="wha?", flowchart, type='list') =>
   return flowOption.choices[results[name]];
 }
 
+// SELECT FLOWCHART
 const selectFlowchart = async () => {
   const foo = await prompts([
     {
       name: 'selectFlowchart',
       type: 'list',
       message: 'Choose a flowchart',
-      choices: ["a", "b", "c"]
+      choices: ['motivation', 'weirdRoom']
     },
   ]);
 
   return foo.selectFlowchart;
 }
 
-
+// MAIN
 const main = async () => {
   while (true) {
     const flowChartSelect = await selectFlowchart();
---
+++ cmd-tool-template/sets/index.js
@@ -0,0 +1,7 @@
+const motivation = require('./motivation.json');
+const weirdRoom = require('./weirdRoom.json');
+
+module.exports = {
+  motivation,
+  weirdRoom
+};
